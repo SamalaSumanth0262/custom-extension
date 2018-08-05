3d49f592c265f695a7e4949df6744763ba69de23
SAMALA SUMANTH <chintusamala96@gmail.com>
2018-08-05 17:27:55 +0530

    added_motivation

--- js/content.js
+++ js/content.js
@@ -1,34 +1,39 @@
 $(document).ready(function() {
     $("#todo_list").html(localStorage.getItem('listItems'));
+    $("#replica").html(localStorage.getItem('all_notes'));
+
+
     $('.add_items').submit(function(event){
       event.preventDefault();
       var item = $("#todo_list_item").val();
 
       if(item)
       {
-        $("#todo_list").append("<li class='todo-rouding'>"+item+"<a id='completed'><i class=\"fas fa-check\"></i>\n" +
+        $("#todo_list").append("<li class='todo-rouding'>"+item+"<a id='completed' alt='add'><i class=\"fas fa-check\"></i>\n" +
           "\n</a><a id='remove'><i class=\"far fa-trash-alt\"></i>\n" + "\n</a></li>");
         localStorage.setItem('listItems',$('#todo_list').html());
         $("#todo_list_item").val("");
       }
 
     });
+
     $(document).on('click', '#completed', function(){
       $(this).parent().css("text-decoration","line-through");
       localStorage.setItem('listItems', $('#todo_list').html());
     });
+
     // REMOVE JUQERY
     $(document).on('click', '#remove', function(){
       $(this).parent().remove();
       localStorage.setItem('listItems', $('#todo_list').html());
 
     });
-    //cloning the list
-    $(document).on('click','#add_button',function(){
-      $("#todo-box").clone().appendTo("body");
-    });
+
+
+
 
 });
+
 
 
 
